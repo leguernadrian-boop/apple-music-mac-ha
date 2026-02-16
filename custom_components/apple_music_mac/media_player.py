cc476184ecb3f2f37a22bb12c9e3a8e3d1fa7780
leguernadrian-boop <leguernadrian@icloud.com>
2026-02-16 02:23:26 +0100

    Update media_player.py

--- custom_components/apple_music_mac/media_player.py
+++ custom_components/apple_music_mac/media_player.py
@@ -1,53 +1,57 @@
 import requests
+import logging
+import voluptuous as vol
 from homeassistant.components.media_player import MediaPlayerEntity
 from homeassistant.components.media_player.const import (
     SUPPORT_PLAY, SUPPORT_PAUSE, SUPPORT_NEXT_TRACK, 
     SUPPORT_PREVIOUS_TRACK, SUPPORT_VOLUME_SET
 )
 
+_LOGGER = logging.getLogger(__name__)
+
 async def async_setup_entry(hass, config_entry, async_add_entities):
     host = config_entry.data.get("host")
     port = config_entry.data.get("port")
-    async_add_entities([AppleMusicModern(host, port)])
+    async_add_entities([AppleMusicModern(host, port)], update_before_add=True)
 
 class AppleMusicModern(MediaPlayerEntity):
     def __init__(self, host, port):
         self._url = f"http://{host}:{port}"
-        self._name = "Apple Music for Mac"
+        self._attr_name = "Apple Music Mac"
         self._state = None
         self._title = None
         self._artist = None
-
-    @property
-    def name(self): return self._name
+        self._volume = 0
 
     @property
     def state(self): return self._state
-
     @property
     def media_title(self): return self._title
-
     @property
     def media_artist(self): return self._artist
-
+    @property
+    def volume_level(self): return self._volume
     @property
     def supported_features(self):
-        # On active Play, Pause, Suivant, Précédent et Volume
         return SUPPORT_PLAY | SUPPORT_PAUSE | SUPPORT_NEXT_TRACK | SUPPORT_PREVIOUS_TRACK | SUPPORT_VOLUME_SET
 
     def update(self):
-        # On demande les infos au serveur de ton Mac
         try:
             r = requests.get(f"{self._url}/now_playing", timeout=5)
             data = r.json()
             self._title = data.get("name")
             self._artist = data.get("artist")
+            self._volume = data.get("volume", 0) / 100
             state = data.get("player_state")
             self._state = "playing" if state == "playing" else "paused"
-        except:
+        except Exception as e:
+            _LOGGER.error("Erreur de connexion au Mac : %s", e)
             self._state = "off"
 
     def media_play(self): requests.put(f"{self._url}/play")
     def media_pause(self): requests.put(f"{self._url}/pause")
     def media_next_track(self): requests.put(f"{self._url}/next")
     def media_previous_track(self): requests.put(f"{self._url}/previous")
+    def set_volume_level(self, volume):
+        level = int(volume * 100)
+        requests.put(f"{self._url}/volume", json={"level": level})
