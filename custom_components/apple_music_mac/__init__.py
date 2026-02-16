c76ce47bb6fec9850f42d320ed5ebbc137a56dae
leguernadrian-boop <leguernadrian@icloud.com>
2026-02-16 02:51:23 +0100

    Update __init__.py

--- custom_components/apple_music_mac/__init__.py
+++ custom_components/apple_music_mac/__init__.py
@@ -1,9 +1,7 @@
 async def async_setup(hass, config):
-    """Configuration via YAML (non utilisée)."""
     return True
 
 async def async_setup_entry(hass, entry):
-    """Configuration via l'interface."""
-    # On ajoute un 's' à setups et on met le domaine dans une liste []
+    # C'est ici qu'il faut le 's' à setups
     await hass.config_entries.async_forward_entry_setups(entry, ["media_player"])
     return True
